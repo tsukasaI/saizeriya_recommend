a77cf2a3d0fa5e10f3b273a1c6eb185a2980baf4
tsukasaI <inoue.tsukasa.4@gmail.com>
2023-03-28 21:16:22 +0900

    add yaml and show menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import saizeriyaMenu from './assets/menu.yaml'
 
-function App() {
+const App: React.FC= () => {
   const [count, setCount] = useState(0)
+  const menu = LoadMenu()
+  console.log(menu)
 
   return (
     <div className="App">
@@ -12,8 +15,28 @@ function App() {
           count is {count}
         </button>
       </div>
+      <div>
+        {menu.map(m => {
+          return (
+            <>
+              <h2>{m.code}</h2>
+              <p>{m.name}</p>
+              <p>{m.price}</p>
+            </>
+          )
+        })}
+      </div>
     </div>
   )
+}
+
+type menu = {
+  code: string
+  name: string
+  price: number
+}
+const LoadMenu = () => {
+  return saizeriyaMenu.grand as menu[]
 }
 
 export default App
