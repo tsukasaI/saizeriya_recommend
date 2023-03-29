546805d66517d4cb76a8d930a2fb7816e8d486b7
tsukasaI <inoue.tsukasa.4@gmail.com>
2023-03-29 23:45:02 +0900

    fix html

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import saizeriyaMenu from './assets/menu.yaml'
 
-const App: React.FC= () => {
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
   const menu = LoadMenu()
   console.log(menu)
 
   return (
     <div className="App">
-        <h1>Saizeriya Recommend</h1>
+      <h1>Saizeriya Recommend</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      <div>
-        {menu.map(m => {
+      <ul>
+        {menu.map((m) => {
           return (
-            <>
-              <h2>{m.code}</h2>
-              <p>{m.name}</p>
-              <p>{m.price}</p>
-            </>
+            <li>
+              <b>{m.code}</b>
+              <span>
+                : {m.name} {m.price}円
+              </span>
+            </li>
           )
         })}
-      </div>
+      </ul>
     </div>
   )
 }
