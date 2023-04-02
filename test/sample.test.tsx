c8c219fb87ba77dbe5941e39efa3479884541ee9
tsukasaI <inoue.tsukasa.4@gmail.com>
2023-04-02 11:58:29 +0900

    menu list partially

--- test/sample.test.tsx
+++ test/sample.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, expectTypeOf } from 'vitest'
 import { GrandMenuList } from '../src/components/MenuList'
 import { render, screen } from '@testing-library/react'
+import { LoadGrandMenu } from '../src/api/menu'
+import { menu } from '../src/model/menu'
 
-describe('sample', () => {
-  test('two plus two is four', () => {
-    expect(2 + 2).toBe(4)
+describe('GrandMenuList', () => {
+  test('parse yaml grand menu', () => {
+    const result = LoadGrandMenu()
+    expectTypeOf(result).items.toEqualTypeOf<menu>()
   })
 
   test('renders h1 text', () => {
