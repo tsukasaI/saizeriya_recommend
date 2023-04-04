68de83a179841c41bb8c5613e936bffae309562d
tsukasaI <inoue.tsukasa.4@gmail.com>
2023-04-04 09:24:49 +0900

    add recommend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
-import { GrandMenuList } from './components/MenuList'
+import { MenuList } from './components/MenuList'
 import { Recommend } from './components/Recommend'
+import { LoadGrandMenu } from './api/menu'
 
 const App: React.FC = () => {
+  const grandMenu = LoadGrandMenu()
+
   return (
     <div className="App">
       <h1>Saizeriya Recommend</h1>
-      <GrandMenuList />
+      <h2>グランドメニュー</h2>
+      <MenuList menu={grandMenu} />
       <Recommend />
     </div>
   )
--- src/components/Recommend.tsx
+++ src/components/Recommend.tsx
@@ -1,24 +1,26 @@
-import { useRef, useState } from "react";
-import { recommend } from "../features/Recommend";
+import { useRef, useState } from 'react'
+import { recommend } from '../features/Recommend'
+import { menu } from '../models/menu'
+import { MenuList } from './MenuList'
 
 export const Recommend = () => {
-  console.log("rendered");
-  const [warning, setWarning] = useState("")
-  const priceRef = useRef<HTMLInputElement>(null);
+  const [warning, setWarning] = useState('')
+  const [recommendMenu, setRecommendMenu] = useState<menu[]>()
+  const priceRef = useRef<HTMLInputElement>(null)
   const handleChange = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const price = Number(priceRef.current?.value);
-    recommend()
-
-  };
+    const price = Number(priceRef.current?.value)
+    const recommended = recommend(price)
+    setRecommendMenu(() => recommended)
+  }
   const handleChangePrice = () => {
-    const currentInputNumber = Number(priceRef.current?.value);
+    const currentInputNumber = Number(priceRef.current?.value)
     if (Number.isNaN(currentInputNumber)) {
-      setWarning(() => "数字で入力してください")
-      priceRef.current!.value = "1000";
+      setWarning(() => '数字で入力してください')
+      priceRef.current!.value = '1000'
       return
     }
-    setWarning(() => "")
-  };
+    setWarning(() => '')
+  }
 
   return (
     <>
@@ -31,8 +33,17 @@ export const Recommend = () => {
         onChange={handleChangePrice}
       />
       <div className="card">
-        <button onClick={handleChange}>ランダムメニューを見る</button>
+        <button onClick={handleChange}>予算を決定</button>
+        <ul>
+          {recommendMenu && <MenuList menu={recommendMenu} />}
+        </ul>
+        <p>
+          合計:{' '}
+          {recommendMenu &&
+            recommendMenu.reduce((acc, cur) => acc + cur.price, 0)}
+          円
+        </p>
       </div>
     </>
-  );
-};
+  )
+}
